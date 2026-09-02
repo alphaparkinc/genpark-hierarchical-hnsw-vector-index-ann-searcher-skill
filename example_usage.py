da06446c1adcd5c4b0412c6257d1aab77564fdaf
GenPark <135203240+alphaparkinc@users.noreply.github.com>
2026-09-02 14:34:31 +0800

    feat: add example_usage.py

--- example_usage.py
+++ example_usage.py
@@ -4,7 +4,7 @@ def main():
     client = HierarchicalHnswVectorIndexAnnSearcherClient()
     res = client.query_ann_neighbors(768, 8, 48)
     print('HNSW Vector ANN Searcher: ' + res['search_operation_id'] + ' (Index Size: ' + str(res['index_vectors_count']) + ' vectors)')
-    print('Neighbors: ' + str(res['nearest_neighbors_returned_count']) + ' | Query Latency: ' + str(res['query_latency_micros']) + 'µs | Recall: ' + str(res['recall_rate_estimate_pct']) + '%')
+    print('Neighbors: ' + str(res['nearest_neighbors_returned_count']) + ' | Query Latency: ' + str(res['query_latency_micros']) + 'us | Recall: ' + str(res['recall_rate_estimate_pct']) + '%')
     print('Index Manifest: ' + res['vector_index_manifest_url'])
 
 if __name__ == '__main__':
